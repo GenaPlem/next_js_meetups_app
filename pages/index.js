b76564f9ffdc1c0b7be4b4bbb508995db2438557
GenaPlem <shainkill1337@gmail.com>
2024-12-02 13:04:52 +0000

    Add Home page with dummy meetups data

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/M%C3%BCnchen_Panorama-CN.jpg/1280px-M%C3%BCnchen_Panorama-CN.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/M%C3%BCnchen_Panorama-CN.jpg/1280px-M%C3%BCnchen_Panorama-CN.jpg",
+    address: "Some address 25, 12456 Some City",
+    description: "This is a second meetup!",
+  },
+];
+
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
