e2ef9acf0b4f67b9c0ad45e3e0bf5d22654d9d52
GenaPlem <shainkill1337@gmail.com>
2024-12-03 12:37:01 +0000

    Add dynamic Meetup Details page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/M%C3%BCnchen_Panorama-CN.jpg/1280px-M%C3%BCnchen_Panorama-CN.jpg"
+      title="A First Meetup"
+      address="Some Street 5, 12345 City"
+      description="The meetup details"
+    />
+  );
+}
