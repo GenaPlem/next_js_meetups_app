018a8a2e331f644976d1819325d2da10b2c81e22
GenaPlem <shainkill1337@gmail.com>
2024-12-04 10:34:00 +0000

    Update Home page with getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -23,22 +23,22 @@ export default function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
-// export async function getStaticProps() {
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//     revalidate: 60,
-//   };
-// }
-
-export async function getServerSideProps(context) {
-  const req = context.req;
-  const res = context.res;
-
+export async function getStaticProps() {
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
+    revalidate: 60,
   };
 }
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
