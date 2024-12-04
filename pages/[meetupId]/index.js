c96f319b4fcbf457e5f60c73ff7b284acfe282aa
GenaPlem <shainkill1337@gmail.com>
2024-12-04 10:34:24 +0000

    Add getStaticProps to Meetup details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,3 +10,20 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/M%C3%BCnchen_Panorama-CN.jpg/1280px-M%C3%BCnchen_Panorama-CN.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 5, 12345 City",
+        description: "The meetup details",
+      },
+    },
+  };
+}
